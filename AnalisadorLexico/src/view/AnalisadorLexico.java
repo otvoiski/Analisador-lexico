2dae11c44cb3c5cd31c97d05adb34f18d9379114
Otavio Augustus <otvoiski@live.com>
2018-05-13 01:48:17 -0300

    Desenvolvido 65% da analise Lexica

--- AnalisadorLexico/src/view/AnalisadorLexico.java
+++ AnalisadorLexico/src/view/AnalisadorLexico.java
@@ -1,6 +1,7 @@
 package view;
 
-import model.Analise;
+import java.util.ArrayList;
+import model.*;
 
 /**
  *
@@ -14,8 +15,24 @@ public class AnalisadorLexico {
     public static void main(String[] args) {
         // TODO code application logic here        
         
-        //Leitura de um arquivo texto        
-        model.Analise analisador = new Analise("codigo/CodigoFonteLexico.txt");        
+        //Leitura de um arquivo texto          
+        Analise analisador = new Analise("codigo/CodigoFonteLexico.txt");
+                
+        ArrayList<Token> Tokens = analisador.Analisar();
+        
+        if (Tokens != null) 
+            for (Token token : Tokens) {                                        //Imprime como solicitado
+                System.out.print(analisador.getExpressao() + " -> ");           //Usa a espressão encontrada
+                
+                    System.out.print
+                        ("[" + token.getClasse() + ","
+                            + token.getRepresentacao() + ","
+                            + token.getValor() + ","
+                            + token.getApontador() + ","
+                            + token.getIndtab() + "]"
+                        );
+                
+            }        
+        System.out.println("");
     }
-    
 }
